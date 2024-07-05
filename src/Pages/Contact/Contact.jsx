fb8e2480c4933390b05860ae2516b0639500d5be
Focus4Study <f4studies@gmail.com>
2024-07-05 23:39:42 +0600

    Banner added to  contact page

---
+++ src/Pages/Contact/Contact.jsx
@@ -0,0 +1,15 @@
+import Banner from "../../Shared/Banner";
+
+
+const Contact = () => {
+    return (
+        <div>
+            <Banner
+            heading='Let’s Make Every Meal a Memorable One'
+            subHeading='Get in Touch'
+            ></Banner>
+        </div>
+    );
+};
+
+export default Contact;
