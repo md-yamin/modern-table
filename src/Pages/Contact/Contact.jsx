e7bcfcf55e8590cb2cc1d45adcd41cb1499cc622
Focus4Study <f4studies@gmail.com>
2024-07-05 23:47:20 +0600

    contact page content added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -10,6 +10,13 @@ const Contact = () => {
             subHeading='Get in Touch'
             bgImgUrl='https://i.ibb.co/Dkd0WzG/2149141352.jpg'
             ></Banner>
+            <h1 className="text-4xl text-center
+            ">Contact US</h1>
+            <div className="flex justify-between text-center mt-10 container mx-auto">
+                <p>Send a Message</p>
+                <p>Our Location</p>
+                <p>Call Us</p>
+            </div>
             <TableBooking></TableBooking>
         </div>
     );
